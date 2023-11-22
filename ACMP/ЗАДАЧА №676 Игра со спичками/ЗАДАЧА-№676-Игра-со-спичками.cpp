d4386cc594aaaf611dcd3a07b7d0f8c10c5b922b
1Zholdoshbek <zholdoshbektashiev@gmail.com>
2023-11-22 21:11:31 +0600

    [200~ЗАДАЧА №676

---
+++ ACMP/ЗАДАЧА №676 Игра со спичками/ЗАДАЧА-№676-Игра-со-спичками.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n<=2){
+        cout<<1;
+    }
+    else if(n%3==0){
+    cout<<2;
+        
+    }
+    else{
+        cout<<1;
+    }
+    
+    return 0;
+}
